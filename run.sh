3bd1fe9b0cfbe2fc795433e66564b71bfcd5d9af
fmaste <fmaste@users.noreply.github.com>
2011-12-16 16:09:51 -0300

    Run is system dependent

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-java -Dwebdriver.chrome.driver=lib/chromedriver-current -jar lib/selenium-server-current
+chromedriver=""
+unamestr=`uname`
+unamearch=`uname -m`
+if [[ "$unamestr" == 'Linux' ]]; then
+	if [[ "$unamearch" == 'x86_64' ]]; then
+		chromedriver='chromedriver-linux32'
+	else
+		chromedriver='chromedriver-linux64'
+	fi
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   chromedriver='chromedriver-osx'
+fi
+
+echo $chromedriver
+
+java -Dwebdriver.chrome.driver=chromedriver/current/$chromedriver -jar selenium/current/selenium-server-standalone.jar
 
